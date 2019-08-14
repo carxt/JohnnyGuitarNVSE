443938c3e4343ba6107eaefe4cf056865dc9a679
carxt <50330670+carxt@users.noreply.github.com>
2019-08-14 21:49:22 +0200

    Update JohnnyGuitarNVSE.cpp

--- JohnnyGuitarNVSE.cpp
+++ JohnnyGuitarNVSE.cpp
@@ -44,7 +44,7 @@ bool NVSEPlugin_Query(const NVSEInterface * nvse, PluginInfo * info)
 	gLog.Open("JohnnyGuitarNVSE.log");
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "JohnnyGuitarNVSE";
-	info->version = 1;
+	info->version = 2;
 
 	if (nvse->isNogore) 
 	{
