d6edb0001fe1bb9175310d064e774160525c0245
carxt <carxtnexus@gmail.com>
2020-05-18 11:41:24 +0200

    Updated Event Handler framework
    Can use generic types now

---
+++ JohnnyGuitar/EventFilteringInterface.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <unordered_set>
+
+union GenericFilters
+{
+	void* ptr;
+	TESForm* form;
+	UInt32        refID;
+	int            intVal;
+	float        fltVal;
+	char* str;
+	GenericFilters()
+	{
+	}
+	GenericFilters(void* ptr)
+	{
+		this->ptr = ptr;
+	}
+	GenericFilters(TESForm* form)
+	{
+		this->form = form;
+	}
+	GenericFilters(UInt32 refID)
+	{
+		this->refID = refID;
+	}
+	GenericFilters(int intVal)
+	{
+		this->intVal = intVal;
+	}
+	GenericFilters(float fltVal)
+	{
+		this->fltVal = fltVal;
+	}
+	GenericFilters(char* str)
+	{
+		this->str = str;
+	}
+
+};
+
+class EventHandlerInterface
+{
+public:
+	//Framework passes the objects to add to filter here
+	GenericFilters* GenFilters = 0;
+	//Used to know how many filters in total (aka the size of the GenericFilters array) the filter uses
+	UInt32 numFilters = 0;
+	//Default destructor
+	virtual ~EventHandlerInterface() {};
+	//When the framework passes filters, it passes them to the GenFilters array pointer, specifying the number of filters in the numFilters member
+	//This function is called by the framework so you can add the objects inside a struct more suitable for search, such as an unordered set
+	virtual void SetUpFiltering() = 0;
+	//Checks if an object is in the filter, recommended to use a fast lookup data structure
+	virtual bool IsInFilter(UInt32 filterNum, GenericFilters toSearch) = 0;
+	//Inserts the desired element to the Nth filter.
+	virtual void InsertToFilter(UInt32 filterNum, GenericFilters toInsert) = 0;
+	//Deletes an object from the Nth filter
+	virtual void DeleteFromFilter(UInt32 filterNum, GenericFilters toDelete) = 0;
+	//Returns if the filter is empty
+	virtual bool IsFilterEmpty(UInt32 filterNum) = 0;
+	//Used by the framework to check if the Nth filter equals the passed value. Useful to avoid adding the same event repeatedly
+	virtual bool IsFilterEqual(GenericFilters Filter, UInt32 filterNum) = 0;
+	//Function used by the filter to check if the object passed is an accepted parameter
+	virtual bool IsAcceptedParameter(GenericFilters toCheck) = 0;
+
+};
+
+
+class EventContainerInterface
+{
+public:
+	void virtual RegisterEvent(Script* script, void** filters);
+	void virtual RemoveEvent(Script* script, void** filters);
+};
+
+
+EventContainerInterface* (_cdecl* CreateScriptEvent)(const char* EventName, UInt8 maxArgs, UInt8 maxFilters, void* (__fastcall* CustomConstructor)( void**, UInt32) );
+void (__cdecl* FreeScriptEvent)(EventContainerInterface*& toRemove);
--- JohnnyGuitar/JohnnyEvents.h
+++ JohnnyGuitar/JohnnyEvents.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "JohnnyGuitar/JohnnyEventInterface.h"
+
+
 
 EventInformation* OnDyingHandler;
 
 void __stdcall handleDyingEvent(TESObjectREFR* thisObj) {
 	for (auto const& callback : OnDyingHandler->EventCallbacks) 
-		if (callback->IsBaseInFilter(0, thisObj)) // 0 is filter one, and we only use an argument so we don't need to check further filters
+		if (callback.eventFilter->IsBaseInFilter(0, thisObj)) // 0 is filter one, and we only use an argument so we don't need to check further filters
 		{
-			FunctionCallScript(callback->ScriptForEvent, NULL, 0, &EventResultPtr, OnDyingHandler->numMaxArgs, thisObj);
+			FunctionCallScript(callback.ScriptForEvent, NULL, 0, &EventResultPtr, OnDyingHandler->numMaxArgs, thisObj);
 		}
 }
 
@@ -26,14 +27,59 @@ __declspec (naked) void OnDyingEventAsm()
 	}
 }
 
+
+
+
+
+
+ParamInfo kParamsJohnnyEventOnDying[7] =
+{
+	{ "setOrRemove", kParamType_Integer, 0 },
+	{ "Script", kParamType_AnyForm, 0 },
+	{ "flags", kParamType_Integer, 1 },
+	{ "Form", kParamType_AnyForm, 1 },
+	{ "Form", kParamType_AnyForm, 1 },
+	{ "Form", kParamType_AnyForm, 1 },
+	{ "Form", kParamType_AnyForm, 1 }
+
+};
+
+DEFINE_COMMAND_PLUGIN(SetJohnnyOnDyingEventHandlerScript, , 0, 7, kParamsJohnnyEventOnDying);
+
+bool Cmd_SetJohnnyOnDyingEventHandlerScript_Execute(COMMAND_ARGS)
+{
+	UInt32 setOrRemove = 0;
+	Script* script = NULL;
+	TESForm* filter[1] = { NULL }; // you always need to make a array of pointers the size of the maximum arguments in the filter, it doesn't matter if most are empty. Framework caveat.
+	UInt32 flags = 0;
+	if (!(ExtractArgs(EXTRACT_ARGS, &setOrRemove, &script, &flags, &filter[0]) || !IS_TYPE(script, Script))) return true;
+	{
+		if (OnDyingHandler)
+		{
+			setOrRemove ? OnDyingHandler->RegisterEvent(script, (void**)filter) : OnDyingHandler->RemoveEventFromGame(script, (void**)filter);
+
+		}
+		return true;
+	}
+}
+
+
+
+
+
+
+
 void initEventHooksAndFunctions(const NVSEInterface* nvse)
 {
-	REG_CMD(RegisterEventHandlerScript);
-	REG_CMD(UnregisterEventHandlerScript);
-	OnDyingOriginalCall = RetrieveAddrFromDisp32Opcode(0x8A1898);
-	WriteRelCall(0x8A1898, (UINT)OnDyingEventAsm);
-	OnDyingHandler = DefineEvent("OnDying", 1, 1, NULL, NULL);
 
+	REG_CMD(SetJohnnyOnDyingEventHandlerScript);
+	if (!(nvse->isEditor))
+	{
+		OnDyingHandler = JGCreateEvent("OnDying", 1, 1, NULL);
+		FunctionCallScript = g_script->CallFunction;
+		OnDyingOriginalCall = RetrieveAddrFromDisp32Opcode(0x8A1898);
+		WriteRelCall(0x8A1898, (UINT)OnDyingEventAsm);
+	}
 
 }
 
