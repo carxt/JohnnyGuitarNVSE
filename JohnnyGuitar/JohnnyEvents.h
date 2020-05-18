47a1289a63c3f336074c2db045a4ee5df7cf9fd7
carxt <carxtnexus@gmail.com>
2020-05-18 12:43:42 +0200

    Feature add
    Added OnStartQuest/OnStopQuest handlers.

--- JohnnyGuitar/JohnnyEvents.h
+++ JohnnyGuitar/JohnnyEvents.h
@@ -3,6 +3,9 @@
 
 
 EventInformation* OnDyingHandler;
+EventInformation* OnStartQuestHandler;
+EventInformation* OnStopQuestHandler;
+
 
 void __stdcall handleDyingEvent(TESObjectREFR* thisObj) {
 	for (auto const& callback : OnDyingHandler->EventCallbacks) 
@@ -11,6 +14,19 @@ void __stdcall handleDyingEvent(TESObjectREFR* thisObj) {
 			FunctionCallScript(callback.ScriptForEvent, NULL, 0, &EventResultPtr, OnDyingHandler->numMaxArgs, thisObj);
 		}
 }
+
+
+
+
+void __fastcall handleQuestStartStop(TESQuest* Quest, bool IsStarted) {
+	EventInformation* thisEvent = IsStarted ? OnStartQuestHandler : OnStopQuestHandler;
+	for (auto const& callback : thisEvent->EventCallbacks)
+		if (callback.eventFilter->IsInFilter(0, Quest)) // 0 is filter one, and we only use an argument so we don't need to check further filters
+		{
+			FunctionCallScript(callback.ScriptForEvent, NULL, 0, &EventResultPtr, thisEvent->numMaxArgs, Quest);
+		}
+}
+
 
 static UInt32 OnDyingOriginalCall;
 __declspec (naked) void OnDyingEventAsm()
@@ -27,39 +43,16 @@ __declspec (naked) void OnDyingEventAsm()
 	}
 }
 
-
-
-
-
-
-ParamInfo kParamsJohnnyEventOnDying[7] =
+__declspec (naked) void OnQuestStartStopEventAsm()
 {
-	{ "setOrRemove", kParamType_Integer, 0 },
-	{ "Script", kParamType_AnyForm, 0 },
-	{ "flags", kParamType_Integer, 1 },
-	{ "Form", kParamType_AnyForm, 1 },
-	{ "Form", kParamType_AnyForm, 1 },
-	{ "Form", kParamType_AnyForm, 1 },
-	{ "Form", kParamType_AnyForm, 1 }
-
-};
-
-DEFINE_COMMAND_PLUGIN(SetJohnnyOnDyingEventHandlerScript, , 0, 7, kParamsJohnnyEventOnDying);
-
-bool Cmd_SetJohnnyOnDyingEventHandlerScript_Execute(COMMAND_ARGS)
-{
-	UInt32 setOrRemove = 0;
-	Script* script = NULL;
-	TESForm* filter[1] = { NULL }; // you always need to make a array of pointers the size of the maximum arguments in the filter, it doesn't matter if most are empty. Framework caveat.
-	UInt32 flags = 0;
-	if (!(ExtractArgs(EXTRACT_ARGS, &setOrRemove, &script, &flags, &filter[0]) || !IS_TYPE(script, Script))) return true;
+	__asm
 	{
-		if (OnDyingHandler)
-		{
-			setOrRemove ? OnDyingHandler->RegisterEvent(script, (void**)filter) : OnDyingHandler->RemoveEventFromGame(script, (void**)filter);
-
-		}
-		return true;
+		mov ecx, dword ptr[ebp-4]
+		mov dl, byte ptr[ebp + 8]
+		call handleQuestStartStop
+		mov esp, ebp
+		pop ebp
+		ret 4
 	}
 }
 
@@ -69,16 +62,100 @@ bool Cmd_SetJohnnyOnDyingEventHandlerScript_Execute(COMMAND_ARGS)
 
 
 
+ParamInfo kParamsJohnnyEventOneFormFilter[4] =
+{
+	{ "setOrRemove", kParamType_Integer, 0 },
+	{ "Script", kParamType_AnyForm, 0 },
+	{ "flags", kParamType_Integer, 1 },
+	{ "Form", kParamType_AnyForm, 1 }
+
+};
+
+DEFINE_COMMAND_PLUGIN(SetJohnnyOnDyingEventHandler, , 0, 4, kParamsJohnnyEventOneFormFilter);
+DEFINE_COMMAND_PLUGIN(SetJohnnyOnStartQuestEventHandler, , 0, 4, kParamsJohnnyEventOneFormFilter);
+DEFINE_COMMAND_PLUGIN(SetJohnnyOnStopQuestEventHandler, , 0, 4, kParamsJohnnyEventOneFormFilter);
+
+bool Cmd_SetJohnnyOnDyingEventHandler_Execute(COMMAND_ARGS)
+{
+	UInt32 setOrRemove = 0;
+	Script* script = NULL;
+	TESForm* filter[1] = { NULL }; // you always need to make a array of pointers the size of the maximum arguments in the filter, it doesn't matter if most are empty. Framework caveat.
+	UInt32 flags = 0;
+	if (!(ExtractArgs(EXTRACT_ARGS, &setOrRemove, &script, &flags, &filter[0]) && IS_TYPE(script, Script))) return true;
+	{
+		if (OnDyingHandler)
+		{
+			if (setOrRemove)
+				OnDyingHandler->RegisterEvent(script, (void**)filter);
+			else OnDyingHandler->RemoveEventFromGame(script, (void**)filter);
+
+		}
+		return true;
+	}
+}
+
+
+
+
+bool Cmd_SetJohnnyOnStartQuestEventHandler_Execute(COMMAND_ARGS)
+{
+	UInt32 setOrRemove = 0;
+	Script* script = NULL;
+	TESForm* filter[1] = { NULL }; // you always need to make a array of pointers the size of the maximum arguments in the filter, it doesn't matter if most are empty. Framework caveat.
+	UInt32 flags = 0;
+	if (!(ExtractArgs(EXTRACT_ARGS, &setOrRemove, &script, &flags, &filter[0]) && IS_TYPE(script, Script))) return true;
+	{
+		if (OnStartQuestHandler)
+		{
+			if (setOrRemove)
+				OnStartQuestHandler->RegisterEvent(script, (void**)filter);
+			else OnStartQuestHandler->RemoveEventFromGame(script, (void**)filter);
+
+		}
+		return true;
+	}
+}
+
+
+bool Cmd_SetJohnnyOnStopQuestEventHandler_Execute(COMMAND_ARGS)
+{
+	UInt32 setOrRemove = 0;
+	Script* script = NULL;
+	TESForm* filter[1] = { NULL }; // you always need to make a array of pointers the size of the maximum arguments in the filter, it doesn't matter if most are empty. Framework caveat.
+	UInt32 flags = 0;
+	if (!(ExtractArgs(EXTRACT_ARGS, &setOrRemove, &script, &flags, &filter[0]) && IS_TYPE(script, Script))) return true;
+	{
+		if (OnStopQuestHandler)
+		{
+			if (setOrRemove)
+				OnStopQuestHandler->RegisterEvent(script, (void**)filter);
+			else OnStopQuestHandler->RemoveEventFromGame(script, (void**)filter);
+
+		}
+		return true;
+	}
+}
+
+
+
 void initEventHooksAndFunctions(const NVSEInterface* nvse)
 {
 
-	REG_CMD(SetJohnnyOnDyingEventHandlerScript);
+	REG_CMD(SetJohnnyOnDyingEventHandler);
+	REG_CMD(SetJohnnyOnStartQuestEventHandler);
+	REG_CMD(SetJohnnyOnStopQuestEventHandler);
+
 	if (!(nvse->isEditor))
 	{
 		OnDyingHandler = JGCreateEvent("OnDying", 1, 1, NULL);
+		OnStartQuestHandler = JGCreateEvent("OnStartQuest", 1, 1, NULL);
+		OnStopQuestHandler = JGCreateEvent("OnStopQuest", 1, 1, NULL);
+
 		FunctionCallScript = g_script->CallFunction;
 		OnDyingOriginalCall = RetrieveAddrFromDisp32Opcode(0x8A1898);
 		WriteRelCall(0x8A1898, (UINT)OnDyingEventAsm);
+		WriteRelJump(0x60CA24, (UINT)OnQuestStartStopEventAsm);
+		SafeWrite8(0x60CA29, 0xCC);
 	}
 
 }
