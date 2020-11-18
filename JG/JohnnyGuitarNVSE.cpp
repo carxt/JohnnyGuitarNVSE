78306449721252ac0ee44e9f4726fed2484198d5
Nikolay Malets <maletsna@gmail.com>
2020-11-18 10:34:11 +0300

    release 3.35

--- JG/JohnnyGuitarNVSE.cpp
+++ JG/JohnnyGuitarNVSE.cpp
@@ -74,7 +74,7 @@ bool NVSEPlugin_Query(const NVSEInterface * nvse, PluginInfo * info)
 	gLog.Open("JohnnyGuitarNVSE.log");
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "JohnnyGuitarNVSE";
-	info->version = 330;
+	info->version = 335;
 
 	if (nvse->isNogore) 
 	{
