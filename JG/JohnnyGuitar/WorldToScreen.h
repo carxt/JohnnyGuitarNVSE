0fb4d035be35aa3e576e57768b30d4629c9e68cb
Nikolay Malets <maletsna@gmail.com>
2021-02-04 20:12:08 +0300

    more refactoring

--- JG/JohnnyGuitar/WorldToScreen.h
+++ JG/JohnnyGuitar/WorldToScreen.h
@@ -21,7 +21,6 @@
 		return *this = NiPoint3(x op v.x, y op v.y, z op v.z); \
 	}
 
-
 struct NiPoint3
 {
 	float x, y, z;
@@ -111,8 +110,6 @@ struct NiPoint3
 	}
 };
 
-
-
 struct NiCameraAlt //Defined here because the one in NVSE is wrong.
 {
 	UInt8			undefinedDataStruct[156];
@@ -123,8 +120,6 @@ struct NiCameraAlt //Defined here because the one in NVSE is wrong.
 	NiViewport		m_kPort;			// 100
 	float			LODAdjust;			// 110
 };
-
-
 
 
 struct JGWorldToScreenMatrix
@@ -228,114 +223,6 @@ bool __fastcall WorldToScreenPoint3(JGWorldToScreenMatrix* cam, NiPoint3* kPt, f
 }
 
 
-
-
-
-
-
-
-//Legacy code idc
-
-//I hate my code to be honest.
-/*
-__forceinline void FOVCorrection(float &xOut, float &yOut, float &zOut)
-{
-
-	UInt32 PlayerAddress = *(UInt32 *)0x11DEA3C;
-	float fmult = 1;
-	if (((*(UInt8*)(PlayerAddress + 0x64A)) == 0) && !(*(float*)((*(UInt32*)(0x11F91AC)) + 0x2B4))) { // this should solve it in the meantime, until I properly fix it
-
-		fmult = (fastDTan(0.5 * (*(float *)(PlayerAddress + 0x674))) / (fastDTan((*(float *)(PlayerAddress + 0x670)) * 0.5)));
-	}
-	xOut = 0.5 + ((xOut - 0.5) * fmult);
-	yOut = 0.5 + ((yOut - 0.5) * fmult);
-	zOut = 0.5 + ((zOut - 0.5) * fmult);
-}
-
-
-bool __fastcall WorldToScreenPoint3(NiCameraAlt* cam, NiPoint3* kPt, float &fBx, float &fBy,
-	float &fBz, float fZeroTolerance, float HandleType)
-{
-	bool st = false;
-	// project a world space point to screen space
-	float fW = kPt->x * cam->m_aafWorldToCam[3][0] +
-		kPt->y * cam->m_aafWorldToCam[3][1] + kPt->z * cam->m_aafWorldToCam[3][2] +
-		cam->m_aafWorldToCam[3][3];
-
-	fBx = kPt->x * cam->m_aafWorldToCam[0][0] + kPt->y * cam->m_aafWorldToCam[0][1] +
-		kPt->z * cam->m_aafWorldToCam[0][2] + cam->m_aafWorldToCam[0][3];
-	fBy = kPt->x * cam->m_aafWorldToCam[1][0] + kPt->y * cam->m_aafWorldToCam[1][1] +
-		kPt->z * cam->m_aafWorldToCam[1][2] + cam->m_aafWorldToCam[1][3];
-	if (fW == 0) return false;
-
-	float fInvW = 1.0f / fW;
-	// Transform Z, not entirely sure if it works. This indicates whether you're in front or behind the camera.
-	fBz = kPt->x * cam->m_aafWorldToCam[2][0] + kPt->y *
-		cam->m_aafWorldToCam[2][1] + kPt->z * cam->m_aafWorldToCam[2][2] +
-		cam->m_aafWorldToCam[2][3];
-	fBz = fBz * fInvW;
-
-	if (!(fW > fZeroTolerance))
-	{
-		st = true;
-		fInvW *= -1.0f;
-	}
-
-	fBx = fBx * fInvW;
-	fBy = fBy * fInvW;
-
-	fBx *= (cam->m_kPort.r - cam->m_kPort.l) * 0.5f;
-	fBy *= (cam->m_kPort.t - cam->m_kPort.b) * 0.5f;
-
-	fBx += (cam->m_kPort.r + cam->m_kPort.l) * 0.5f;
-	fBy += (cam->m_kPort.t + cam->m_kPort.b) * 0.5f;
-
-	fBy = cam->m_kPort.t - fBy;
-	FOVCorrection(fBx, fBy, fBz);
-	if (fBx >= cam->m_kPort.l && fBx <= cam->m_kPort.r &&
-		fBy >= cam->m_kPort.b && fBy <= cam->m_kPort.t && st == 0)
-	{
-		return true;
-	}
-	else
-	{
-		if (HandleType < 2)
-		{
-			float x2 = cam->m_kPort.r / 2, y2 = cam->m_kPort.t / 2;
-			float d = sqrt((((fBx - x2)*(fBx - x2))) + ((fBy - y2)*(fBy - y2))); //Distance
-			float r = y2 / d; //Segment ratio
-			fBx = (((r * fBx + (1 - r) * x2)));//find point that divides the segment
-			fBy = (((r * fBy + (1 - r) * y2)));//into the ratio (1-r):r, this yields circle coordinates
-			if (!(HandleType)) //We get square coordinates, since the circle we got is actually a circle inside the square.
-			{
-				x2 = fBx - 0.5; y2 = fBy - 0.5;
-				float Divider = (((y2)* (y2)) > 0.125) ? fabs(y2) : fabs(x2);
-				fBx = 0.5 * (((x2) / Divider) + 1);
-				fBy = 0.5 * (((y2) / Divider) + 1);
-			}
-			//Next two lines of code are not actually necessary at all, I'm just a paranoid fuck
-			fBx = fBx > 0 ? ((fBx < 1) ? fBx : 1) : 0;
-			fBy = fBy > 0 ? ((fBy < 1) ? fBy : 1) : 0;
-		}
-		return false;
-	}
-
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 NiPoint3* NiPointBuffer = NULL;
 
 __forceinline bool WorldToScreen(NiPoint3* p_in, float& x_out, float& y_out, float& z_out, float HandleType, float zeroTolerance = 0.0000099999997)
@@ -370,8 +257,6 @@ void __stdcall CopyNiCamera(NiCameraAlt* MemoryAddressToCopy, float fov)
 }
 
 
-
-
 __declspec(naked) void NiCameraGetAltHook()
 {
 	__asm
@@ -382,29 +267,3 @@ __declspec(naked) void NiCameraGetAltHook()
 		leave
 		retn 0x10
 	}
-
-
-
-}
-
-
-
-/*
-__declspec(naked) void NiCameraGetAltHook()
-{
-	__asm 
-	{
-		sub esp, 4
-		fld dword ptr ss: [ebp + 8]
-		fstp dword ptr ss :[esp]
-		mov eax, dword ptr ss: [ebp-8]
-		push eax
-		call CopyNiCamera
-		mov esp, ebp
-		pop ebp
-		retn 0x10
-	}
-
-
-
-}*/
