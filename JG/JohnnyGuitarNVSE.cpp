095ee8f73ca1e39724968d7543225ae47348e1d9
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2025-09-07 23:31:07 +0200

    Update version to 518

--- JG/JohnnyGuitarNVSE.cpp
+++ JG/JohnnyGuitarNVSE.cpp
@@ -52,7 +52,7 @@ HMODULE JohnnyHandle;
 _CaptureLambdaVars CaptureLambdaVars;
 _UncaptureLambdaVars UncaptureLambdaVars;
 NiTMap<const char*, TESForm*>** g_gameFormEditorIDsMap = reinterpret_cast<NiTMap<const char*, TESForm*>**>(0x11C54C8);
-#define JG_VERSION 517
+#define JG_VERSION 518
 void MessageHandler(NVSEMessagingInterface::Message* msg) {
 	switch (msg->type) {
 		case NVSEMessagingInterface::kMessage_PostPostLoad:
