2c9fc47e6b841090d7b1cb9d4a7b20bb4799aace
Stentorious <124759154+Stentorious@users.noreply.github.com>
2025-10-07 03:34:17 -0400

    Add optional dontCache arg to PlaySoundFromPath and PlaySound3DFromPath

--- nvse/nvse/ParamInfos.h
+++ nvse/nvse/ParamInfos.h
@@ -77,6 +77,15 @@ static ParamInfo kParams_OneString_OneOptionalFloat_ThreeOptionalInts[5] =
 	{ "integer", kParamType_Integer, 1 },
 	{ "integer", kParamType_Integer, 1 },
 };
+static ParamInfo kParams_OneString_OneOptionalFloat_FourOptionalInts[6] =
+{
+	{ "string",	kParamType_String,	0 },
+	{ "float", kParamType_Float, 1 },
+	{ "integer", kParamType_Integer, 1 },
+	{ "integer", kParamType_Integer, 1 },
+	{ "integer", kParamType_Integer, 1 },
+	{ "integer", kParamType_Integer, 1 },
+};
 
 static ParamInfo kParams_TwoFloats[2] =
 {
