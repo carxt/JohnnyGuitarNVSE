2582d63e79a92bcfcadef2b8dc4111e9ff7b59b9
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2025-11-13 20:42:26 +0100

    Add IsMenuPaused (#105)
    Merging now to set an opcode

--- JG/JohnnyGuitarNVSE.cpp
+++ JG/JohnnyGuitarNVSE.cpp
@@ -569,7 +569,8 @@ extern "C" {
 		REG_CMD(RemoveHighlightedRef);
 		REG_CMD(GetSaidOnce);
 		REG_CMD(SetSaidOnce);
-		REG_TYPED_CMD(GetTopicInfo, Array)
+		REG_TYPED_CMD(GetTopicInfo, Array);
+		REG_CMD(IsMenuPaused);
 		g_scriptInterface = (NVSEScriptInterface*)nvse->QueryInterface(kInterface_Script);
 		g_cmdTableInterface = (NVSECommandTableInterface*)nvse->QueryInterface(kInterface_CommandTable);
 		s_strArgBuf = (char*)malloc((sizeof(char)) * 1024);
