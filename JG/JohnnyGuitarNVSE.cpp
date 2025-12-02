909b4330036f3ba7d45e56ce87f66dd4d02a1779
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2025-12-02 22:05:41 +0100

    Fix opcode order

--- JG/JohnnyGuitarNVSE.cpp
+++ JG/JohnnyGuitarNVSE.cpp
@@ -573,12 +573,12 @@ extern "C" {
 		REG_CMD(GetSaidOnce);
 		REG_CMD(SetSaidOnce);
 		REG_TYPED_CMD(GetTopicInfo, Array)
-		REG_CMD(SetHUDVisibilityOverride);
-		REG_CMD(GetHUDVisibilityOverride);
-
 		REG_TYPED_CMD(GetTopicInfo, Array);
 		REG_CMD(IsMenuPaused);
 		REG_CMD(IsInDialogueWithPlayer);
+		REG_CMD(SetHUDVisibilityOverride);
+		REG_CMD(GetHUDVisibilityOverride);
+		
 		g_scriptInterface = (NVSEScriptInterface*)nvse->QueryInterface(kInterface_Script);
 		g_cmdTableInterface = (NVSECommandTableInterface*)nvse->QueryInterface(kInterface_CommandTable);
 		s_strArgBuf = (char*)malloc((sizeof(char)) * 1024);
