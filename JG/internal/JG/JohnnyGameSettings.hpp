1d722219225f60cf29a8535c7a9ed5bbe60fd84c
c6 <31777460+c6-dev@users.noreply.github.com>
2026-03-21 23:35:33 +0400

    remove unused consts

--- JG/internal/JG/JohnnyGameSettings.hpp
+++ JG/internal/JG/JohnnyGameSettings.hpp
@@ -2,8 +2,6 @@
 #include <GameSettings.h>
 
 namespace JohnnyGameSettings {
-	constexpr uintptr_t func_AddGameSetting_Float = 0x040E0B0;
-	constexpr uintptr_t func_AddGameSetting_IntOrStaticStr = 0x40C150;
 
 	extern CustomGameSetting fCombatLocationTargetRadiusMaxBase;
 	extern CustomGameSetting fCombatRangedWeaponRangeBaseMult;
