a928bdef4dbd8e512b4e85f679690c96bbb1be88
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2026-04-29 09:56:09 +0200

    Update3DAlt - improve actor handling (#143)

--- JG/functions/fn_form.cpp
+++ JG/functions/fn_form.cpp
@@ -10,6 +10,7 @@
 #include <unordered_map>
 #include "JG/JGSetList.hpp"
 #include <JG/BarterFilter.hpp>
+#include <JG/JohnnyExtraData.hpp>
 #include <GameData.h>
 #include <GameRTTI.h>
 #include "decoding.h"
@@ -2528,7 +2529,7 @@ enum UPDATE3D_FLAGS_EX {
 	UPDATE_POS		= 1u << 6,
 };
 
-void RefreshReferenceModel(TESObjectREFR* apReference, uint32_t auiFlags) {
+static void __fastcall RefreshReferenceModel(TESObjectREFR* apReference, uint32_t auiFlags) {
 	if (auiFlags & UPDATE_MODEL) {
 		apReference->Update3D();
 		ThisCall(0x456520, *reinterpret_cast<DWORD**>(0x1202D98));
@@ -2545,53 +2546,63 @@ void RefreshReferenceModel(TESObjectREFR* apReference, uint32_t auiFlags) {
 		ShadowSceneNode* pSSN = BSShaderManager::GetShadowSceneNode(BSShaderManager::SceneGraphType::WORLD);
 		pSSN->UpdateObjectLighting(apReference->Get3DSimple(), false);
 	}
+
+	if (auiFlags & UPDATE_POS) {
+		apReference->Update3DPosition();
+	}
+}
+
+static void __fastcall RequestModelUpdate(TESObjectREFR* apReference, uint32_t auiFlags, bool abQueue) {
+	if (abQueue) {
+		JohnnyExtraData* pExtraData = JohnnyExtraData::GetOrCreate(apReference);
+		pExtraData->IncRefCount();
+
+		QueuedTask kTask;
+		kTask.kItems[0].p = pExtraData;
+		kTask.kItems[1].ui = auiFlags;
+		kTask.pFunction = QUEUED_TASK{
+			JohnnyExtraData* pData = reinterpret_cast<JohnnyExtraData*>(arTask.kItems[0].p);
+			TESObjectREFR* pRef = static_cast<TESObjectREFR*>(pData->pOwner);
+			if (pRef) {
+				uint32_t uiFlags = arTask.kItems[1].ui;
+				RefreshReferenceModel(pRef, uiFlags);
+			}
+			pData->DecRefCount();
+		};
+		TaskQueue::QueueTask(kTask);
+	}
+	else {
+		RefreshReferenceModel(apReference, auiFlags);
+	}
 }
 
 bool Cmd_Update3DAlt_Execute(COMMAND_ARGS) {
+	*result = 0;
 	uint32_t uiFlags = 0;
 	if (ExtractArgsEx(EXTRACT_ARGS_EX, &uiFlags) && uiFlags) {
-		bool bQueue = AILinearTaskThreadManager::ShouldQueue3DTask();
-		if (thisObj->IsMobileObject()) {
-			MobileObject* pObj = static_cast<MobileObject*>(thisObj);
-			if (pObj->baseProcess) {
-				pObj->baseProcess->Set3DUpdateFlag(uiFlags);
+		if (!thisObj->Get3DSimple() || thisObj->IsStillLoading())
+			return true;
 
-				if (uiFlags & UPDATE_LIGHTS) {
-					ShadowSceneNode* pSSN = BSShaderManager::GetShadowSceneNode(BSShaderManager::SceneGraphType::WORLD);
-					pSSN->UpdateObjectLighting(pObj->Get3DSimple(), false);
+		const bool bQueue = AILinearTaskThreadManager::ShouldQueue3DTask();
+		if (thisObj->IsActor()) {
+			Actor* pActor = static_cast<Actor*>(thisObj);
+			if (pActor->baseProcess) {
+				// Creatures can't refresh their models in vanilla, so we have to handle them ourselves.
+				if (pActor->IsCreature()) {
+					RequestModelUpdate(thisObj, uiFlags, bQueue);
+				}
+				else {
+					pActor->baseProcess->Set3DUpdateFlag(uiFlags);
+
+					constexpr uint32_t uiNPCCustomFlags = UPDATE_LIGHTS | UPDATE_POS;
+					if (uiFlags & uiNPCCustomFlags) {
+						RefreshReferenceModel(thisObj, uiFlags & ~uiNPCCustomFlags);
+					}
 				}
 			}
 		}
 		else {
-			if (bQueue) {
-				QueuedTask kTask;
-				kTask.kItems[0].p = thisObj;
-				kTask.kItems[1].ui = uiFlags;
-				kTask.pFunction = QUEUED_TASK{
-					TESObjectREFR * pRef = reinterpret_cast<TESObjectREFR*>(arTask.kItems[0].p);
-					uint32_t uiFlags = arTask.kItems[1].ui;
-					RefreshReferenceModel(pRef, uiFlags);
-				};
-				TaskQueue::QueueTask(kTask);
-			}
-			else {
-				RefreshReferenceModel(thisObj, uiFlags);
-			}
-		}
-
-		if (uiFlags & UPDATE_POS) {
-			if (bQueue) {
-				QueuedTask kTask;
-				kTask.kItems[0].p = thisObj;
-				kTask.pFunction = QUEUED_TASK{
-					TESObjectREFR * pRef = reinterpret_cast<TESObjectREFR*>(arTask.kItems[0].p);
-					pRef->Update3DPosition();
-				};
-				TaskQueue::QueueTask(kTask);
-			}
-			else {
-				thisObj->Update3DPosition();
-			}
+			RequestModelUpdate(thisObj, uiFlags, bQueue);
 		}
 
 		*result = 1;
