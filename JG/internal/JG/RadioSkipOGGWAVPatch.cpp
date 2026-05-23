053cfd0e1ab58350eab0886db726a0d4895991e0
carxt <carxtnexus@gmail.com>
2026-05-23 18:00:42 +0200

    patch all instances of improper playback

--- JG/internal/JG/RadioSkipOGGWAVPatch.cpp
+++ JG/internal/JG/RadioSkipOGGWAVPatch.cpp
@@ -1,39 +1,54 @@
 #include "RadioSkipOGGWAVPatch.hpp"
 #include "GameForms.h"
-#include <GameObjects.h>
-#include <decoding.h>
-
+#include "GameSound.h"
 
 namespace RadioSkipOGGWAVPatch {
-	ULONGLONG uiLastPipRadioUpdate;
-	void* hk_QueueRadioSkipUpdate()
+	//Fix playback
+	unsigned int BSWin32Audio_GetTimePassed()
 	{
-		auto* pEbp = GetParentBasePtr(_AddressOfReturnAddress(), false);
-		RadioEntry* pRadio = *((decltype(&pRadio))(pEbp + 0x8));
-		RadioEntry* pGlobalRadio = *((decltype(&pGlobalRadio))(0x11DD42C));
-		if (pRadio == pGlobalRadio)
-		{
-			uiLastPipRadioUpdate = GetTickCount64() + 200; //you'd have to be running at 5 fps for this to be an issue
-		}
-		return CdeclCall<void*>(0x453A70);
+		auto pBSWin32Audio = BSWin32Audio::GetSingleton();
+		return ThisCall<unsigned int>(0x63D040, pBSWin32Audio);
 
 	}
-
-	void* __fastcall hk_QueryRadioSkipUpdatePipboy(void* sound, void* edx, int offset)
+	void* hk_QueryRadioSkipUpdate(BSSoundHandle* pSound, unsigned int offset, bool bDoRewind)
 	{
-		if (uiLastPipRadioUpdate > GetTickCount64())
+		unsigned int msToRewind = 50; //the default value added by the game to skip
+		unsigned int setOffset = offset - msToRewind;
+		if (bDoRewind && ((BSWin32Audio_GetTimePassed()) <= setOffset))
 		{
 			return NULL;
 		}
-		return  ThisCall<void*>(0xAD8FD0, sound, offset);
+		return  ThisCall<void*>(0xAD8FD0, pSound, bDoRewind ? setOffset : offset);
+	}
+	void* __fastcall hk_QueryRadioSkipUpdatePipboy(BSSoundHandle* pSound, void* edx, unsigned int offset)
+	{
+
+		return  hk_QueryRadioSkipUpdate(pSound, offset, true);
+	}
+	void* __fastcall hk_QueryRadioSkipUpdatePlaced(BSSoundHandle* pSound, void* edx, unsigned int offset)
+	{
+		auto* _ebp = GetParentBasePtr(_AddressOfReturnAddress(), false);
+		char* path = reinterpret_cast<char*>(_ebp - 0x17C);
+		bool doRewind = strstr(path, ".mp3") == NULL; //the entire issue is the seek code assumes mp3
+		auto toRet = hk_QueryRadioSkipUpdate(pSound, offset, doRewind);
+		return toRet;
 	}
 
+	void* __cdecl hk_AllowOGGPlayBackFilepath(char* pPath, const char* pConstantWAV)
+	{
+		//Radios can actually play OGG directly... but the code only checks for WAV, although later it checks and if it doesn't find a wav, it falls back to an OGG.
+		//This leads to the hacky workaround where you must specify the file path as .wav even though it's an OGG.
+		auto toRet = strstr(pPath, pConstantWAV);
+		if (!toRet) { toRet = strstr(pPath, ".ogg"); }
+		return toRet;
+	}
 
+	void Install() 
+	{
 
-	void Install() {
-
-		WriteRelCall(0x834749, (uint32_t)hk_QueueRadioSkipUpdate);
-		WriteRelCall(0x8337F7, (uint32_t)hk_QueryRadioSkipUpdatePipboy);
+		WriteRelCall(0x8337F7, (uintptr_t)hk_QueryRadioSkipUpdatePipboy);
+		WriteRelCall(0x8354CC, (uintptr_t)hk_QueryRadioSkipUpdatePlaced);
+		WriteRelCall(0x833680, (uintptr_t)hk_AllowOGGPlayBackFilepath);
 
 	}
 
