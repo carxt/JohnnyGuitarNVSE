d025649c3394900ed61a33d3d761b50fc22026d9
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2026-05-27 17:40:17 +0200

    Add more save types to block (#162)
    * Add more save types to block
    
    * Show proper message when trying to quicksave

--- JG/functions/fn_gameplay.cpp
+++ JG/functions/fn_gameplay.cpp
@@ -1423,15 +1423,9 @@ bool Cmd_DisableMuzzleFlashLights_Execute(COMMAND_ARGS) {
 	return true;
 }
 bool Cmd_ToggleDisableSaves_Execute(COMMAND_ARGS) {
-	enum SaveTypeFlags {
-		NORMAL = 1,
-		AUTO   = 2,
-		SYSTEM = 4,
-	};
-
 	BOOL bDisable = TRUE;
-	uint32_t uiTypeFlags = SaveTypeFlags::NORMAL | SaveTypeFlags::AUTO | SaveTypeFlags::SYSTEM;
-	uint8_t ucIndex = scriptObj->GetCompileIndex();
+	uint32_t uiTypeFlags = DisabledSaves::SaveTypeFlags::ALL;
+	const uint8_t ucIndex = scriptObj->GetCompileIndex();
 	*result = 0;
 	if (ucIndex < 0xFF && ExtractArgsEx(EXTRACT_ARGS_EX, &bDisable, &uiTypeFlags)) {
 		DisabledSaves::Toggle(ucIndex, bDisable > 0, uiTypeFlags);
--- JG/internal/JG/DisabledSaves.hpp
+++ JG/internal/JG/DisabledSaves.hpp
@@ -1,6 +1,31 @@
 #pragma once
 
 namespace DisabledSaves {
+
+	struct _SaveTypeBits {
+		enum Bits {
+			NORMAL	= 0,
+			AUTO	= 1,
+			SYSTEM	= 2,
+			FORCED	= 3,
+			QUICK	= 4,
+		};
+	};
+	using SaveTypeBits = _SaveTypeBits::Bits;
+
+	struct _SaveTypeFlags {
+		enum Flags {
+			NORMAL	= 1 << SaveTypeBits::NORMAL,
+			AUTO	= 1 << SaveTypeBits::AUTO,
+			SYSTEM	= 1 << SaveTypeBits::SYSTEM,
+			FORCED	= 1 << SaveTypeBits::FORCED,
+			QUICK	= 1 << SaveTypeBits::QUICK,
+
+			ALL		= NORMAL | AUTO | SYSTEM | FORCED | QUICK,
+		};
+	};
+	using SaveTypeFlags = _SaveTypeFlags::Flags;
+
 	void Init();
 	void Install();
 	void Reset();
