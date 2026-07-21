cbc64c03b29436a45abe55f8e32bc56bd2b5c420
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2026-07-21 22:03:07 +0200

    LandRemapping - cleanup

--- JG/internal/JG/LandRemapping.hpp
+++ JG/internal/JG/LandRemapping.hpp
@@ -4,14 +4,21 @@ class TESWorldSpace;
 class TESObjectLAND;
 
 namespace LandRemapping {
+
 	void Install();
+
 	void Reset();
-	void RemapLand(uint32_t auiLandFormID, TESWorldSpace* apWorld, int16_t asX, int16_t asY);
-	void ReloadModel(TESObjectLAND* apLand, bool abLoad);
+
+	void __fastcall RemapLand(uint32_t auiLandFormID, const TESWorldSpace* apWorld, int16_t asX, int16_t asY);
+
+	void __fastcall ReloadModel(TESObjectLAND* apLand, bool abLoad);
+
+	bool HasDataToSave();
 
 	using WriteFunc = bool (*)(const void*, uint32_t);
 	using ReadFunc = uint32_t(*)(void*, uint32_t);
-	bool HasDataToSave();
-	void SerializeData(WriteFunc writeFunc);
-	void DeserializeData(ReadFunc readFunc);
+
+	void __fastcall SerializeData(WriteFunc writeFunc);
+	void __fastcall DeserializeData(ReadFunc readFunc);
+
 }
