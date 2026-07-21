4552339d9ba7aded860c340f71b5520bf8508a19
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2026-07-21 21:13:14 +0200

    AddItemMessages - consistency pass

--- JG/internal/JG/AddItemMessages.cpp
+++ JG/internal/JG/AddItemMessages.cpp
@@ -2,14 +2,14 @@
 
 namespace AddItemMessages {
 
-	// When adding item lists, allows per-item "Item Added" messages insated of issuing one message for the last item added
+	// When adding item lists, allow per-item "Item Added" messages instead of issuing one message for the last item added
 
-	void __declspec(naked) MessageLoop_Asm() {
-		static constexpr uint32_t uiRet = 0x482201;
-		static constexpr uint32_t uiDestructor = 0x4037D0;
+	SPEC_NAKED void MessageLoop_Asm() {
+		static constexpr uint32_t uiReturnAddr		= 0x482201;
+		static constexpr uint32_t uiDestructorAddr	= 0x4037D0;
 		__asm {
-			call	uiDestructor
-			jmp     uiRet
+			call	uiDestructorAddr
+			jmp     uiReturnAddr
 		}
 	}
 
@@ -19,7 +19,7 @@ namespace AddItemMessages {
 		HookUtils::SafeWriteBuf(0x4824C8, "\x0F\x85\x33\xFD\xFF\xFF");
 		HookUtils::SafeWriteBuf(0x4824D2, "\x0F\x84\x29\xFD\xFF\xFF");
 		HookUtils::SafeWriteBuf(0x4824DE, "\x0F\x85\x1D\xFD\xFF\xFF");
-		HookUtils::WriteRelJump(0x482648, (uintptr_t)MessageLoop_Asm);
+		HookUtils::WriteRelJump(0x482648, MessageLoop_Asm);
 	}
 
 }
