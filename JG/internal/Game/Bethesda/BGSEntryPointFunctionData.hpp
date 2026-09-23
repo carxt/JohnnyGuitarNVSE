8165e2693a97c839241c95fc3e08ef9cc192287d
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2026-09-23 15:15:37 +0200

    BGSEntryPointFunctionData and BGSPerkEntry vtables

--- JG/internal/Game/Bethesda/BGSEntryPointFunctionData.hpp
+++ JG/internal/Game/Bethesda/BGSEntryPointFunctionData.hpp
@@ -13,6 +13,14 @@ public:
 	virtual void						Save(TESFile* apFile);
 	virtual void						Load(TESFile* apFile);
 	virtual void						InitItem(TESForm* apOwner);
+#ifdef EDITOR
+	virtual void						LoadDialog(HWND, uint32_t);
+	virtual void						Func_08(HWND, uint32_t);
+	virtual void						Func_09(int, char* apBuffer, uint32_t auiBufferSize, uint32_t);;
+	virtual bool						Func_10(void*, void*);
+	virtual void						Func_11(void*);
+	virtual void						Func_12(void*, void*, void*);
+#endif
 };
 
 ASSERT_SIZE(BGSEntryPointFunctionData, 0x4);
